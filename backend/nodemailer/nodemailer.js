8e789ed87fc9741b2cafae66f0cc938ee66c9be5
Rawahaahmed23 <rawahaahmed081@gmail.com>
2026-04-03 23:27:52 +0500

    commit

--- backend/nodemailer/nodemailer.js
+++ backend/nodemailer/nodemailer.js
@@ -1,12 +1,22 @@
 const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com',
   port: 587,
- 
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   }
 });
 
-module.exports = transporter
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("❌ Mailer Error:", error.message);
+  } else {
+    console.log("✅ Mailer Ready");
+  }
+});
+
+module.exports = transporter;
